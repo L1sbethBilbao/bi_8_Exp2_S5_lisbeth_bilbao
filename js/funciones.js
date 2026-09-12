b51a2be12397c471ff8f38f7895eb518beb2dffe
L1sbethBilbao <lisbethbilbao1@gmail.com>
2026-09-12 15:49:41 -0300

    Sitio Kivo semana 5: proyecto independiente de la semana 4

---
+++ js/funciones.js
@@ -0,0 +1,4 @@
+/* Scripts e interactividad.
+   Semana 4: la interactividad del Navbar y del Carousel
+   la aporta bootstrap.bundle.min.js (CDN).
+   Este archivo se mantiene para respetar la estructura del proyecto. */
